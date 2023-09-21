fb43375caefd61db91aaa9c326c22c314174550b
spalz <spals@protonmail.com>
2023-09-21 07:19:08 +0600

    fix ts build

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,15 @@
 "use strict";
 
 import { register } from "./register";
-// import controllers from "./controllers";
-// import config from "./config";
-// import routes from "./routes";
-// import services from "./services";
+import controllers from "./controllers";
+import config from "./config";
+import routes from "./routes";
+import services from "./services";
 
 export default {
   register,
-  // config,
-  // routes,
-  // controllers,
-  // services,
+  config,
+  routes,
+  controllers,
+  services,
 };
